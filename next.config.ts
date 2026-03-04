995fceeba936ee1b4c4ecf469fddbbb2e1392d4f
Jason <jasoncruzdeveloper@gmail.com>
2026-03-04 11:43:01 -0600

    attempting to fix error in netlify and versel build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  cacheComponents: true,
+  experimental: {
+    // Corrected property name for Next.js 16
+    allowedRevalidateHeaderKeys: ["192.168.1.245", "localhost:3000"],
+    // Keeping this false to allow your Sanity dynamic fetching
+    cacheComponents: false,
+  },
 };
 
 export default nextConfig;
